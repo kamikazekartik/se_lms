152f62025dcfd4bfc8935273827727fc101112ef
kamikazekartik <sreenivasan.kartik@gmail.com>
2014-03-30 10:36:39 +0530

    Login Handler has been added
    Search module has been replaced by Login Handler module.

---
+++ LMS/src/LoginHandler.java
@@ -0,0 +1,54 @@
+import java.util.List;
+
+//Source file: D:\\lms\\LoginHandler.java
+
+
+public class LoginHandler 
+{
+   private static String userId = null;
+   
+   /**
+    * @roseuid 531622640234
+    */
+   public LoginHandler() 
+   {
+    
+   }
+
+   /**
+    * @param params
+    * @roseuid 53161A260158
+    */
+   public boolean login(String username, String password) 
+   {
+	   DBHandler db = new DBHandler();
+	   LibraryStaff ls = db.dbUserRetrieve("", username, password);
+	   if(ls == null){
+		   return false; //could not log in user
+	   }else{
+		   this.userId = ls.getId();
+		   return true;
+	   }
+	   
+   }
+   
+   public boolean logout(){
+	   if(this.userId == null){
+		   return false; //no user currently logged in
+	   }else{
+		   this.userId = null;
+		   return true;
+	   }
+   }
+   
+   /**
+    * 
+    * @param params
+    * @roseuid 53161A31030B
+    */
+   public String getCurrentUser(){
+	   return this.userId;
+   }
+
+
+}
